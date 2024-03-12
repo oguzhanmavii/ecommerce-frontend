5e312b8de72c225bfe7f38756b923783b0e4a563
OĞUZHAN MAVİ <mavioguz09@gmail.com>
2024-03-12 17:05:25 +0300

    updated frontend commit.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -9,7 +9,7 @@ function Cart(props) {
   useEffect(() => {
     console.log("Cart Items:", cartItems); // Sepetteki ürünlerin değerini konsola yazdır
     fetchCartItems();
-  },);
+  }, []);
 
   const fetchCartItems = async () => {
     try {
@@ -23,9 +23,18 @@ function Cart(props) {
   const removeItemFromCart = async (itemId) => {
     try {
       await axios.delete(`http://localhost:8070/api/cart/${itemId}`);
-      setCartItems(prevItems => prevItems.filter(item => item.id !== itemId));
+      setCartItems(prevItems => prevItems.filter(order => order.products.find(product => product.id !== itemId)));
     } catch (error) {
       console.error('Error removing item from cart:', error);
+    }
+  };
+
+  const handleBuy = async (itemId) => {
+    try {
+      // Burada "Buy" işlemlerini gerçekleştirebilirsiniz, örneğin siparişi onaylama gibi
+      console.log("Buying item with ID:", itemId);
+    } catch (error) {
+      console.error('Error buying item:', error);
     }
   };
 
@@ -35,31 +44,31 @@ function Cart(props) {
   };
 
   return (
-    <center>    <div className="cart-container"> {/* Cart.css'de belirtilen cart-container sınıfını kullan */}
-      <h2>Cart</h2>
-      <p>Your shopping cart:</p>
-      <ul>
-        {cartItems.map(order => (
-          <div key={order.id}>
-            <h3>Order ID: {order.id}</h3>
-            <ul>
-              {order.products.map(product => (
-                <li key={product.id}>
-                   <img className="product-image" src={product.imageUrl} alt={product.name} /><br></br>
-                  <strong>{product.name}</strong> - ${product.price}<tr></tr>
-                  <button onClick={() => removeItemFromCart(product.id)}>Remove</button>
-                 
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </ul>
-      <button onClick={handleCheckout}>Checkout</button>
-      <Link to="/home">Back to Home</Link>
-    </div>
+    <center>
+      <div className="cart-container"> {/* Cart.css'de belirtilen cart-container sınıfını kullan */}
+        <h2>Cart</h2>
+        <p>Your shopping cart:</p>
+        <ul>
+          {cartItems.map(order => (
+            <div key={order.id}>
+              <h3>Order ID: {order.id}</h3>
+              <ul>
+                {order.products.map(product => (
+                  <li key={product.id}>
+                    <img className="product-image" src={product.imageUrl} alt={product.name} /><br></br>
+                    <strong>{product.name}</strong> - ${product.price}<tr></tr>
+                    <button onClick={() => removeItemFromCart(product.id)}>Remove</button>
+                    <button onClick={() => handleBuy(product.id)}>Buy</button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </ul>
+        <button onClick={handleCheckout}>Checkout</button>
+        <Link to="/home">Back to Home</Link>
+      </div>
     </center>
-
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,59 +1,130 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './Home.css'; // Stil dosyasını ekleyin
+import './Home.css';
 
-function Home({ history }) {
+const HomePage = () => {
   const [products, setProducts] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [newProduct, setNewProduct] = useState({
+    name: '',
+    price: '',
+    imageUrl: ''
+  });
+  const [username, setUsername] = useState('');
 
   useEffect(() => {
     fetchProducts();
+    getUsernameFromStorage();
   }, []);
 
   const fetchProducts = async () => {
     try {
-      const response = await axios.get('http://localhost:8070/api/products');
-      setProducts(response.data);
+      const { data } = await axios.get('http://localhost:8070/api/products');
+      setProducts(data);
     } catch (error) {
       console.error('Error fetching products:', error);
     }
   };
 
+  const getUsernameFromStorage = () => {
+    const storedUsername = localStorage.getItem('username');
+    if (storedUsername) {
+      setUsername(storedUsername);
+    }
+  };
+
   const handleLogout = () => {
-    // Kullanıcıyı giriş sayfasına yönlendir
     window.location.href = '/';
   };
 
   const handleAddToCart = (productId) => {
-    // Sepete ekleme işlemi burada yapılacak
-    // Örneğin, bir API isteği gönderilerek ürün sepete eklenebilir
-    // Ardından Cart sayfasına yönlendirilir
     window.location.href='/cart';
   };
 
+  const handleProductSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:8070/api/products', newProduct);
+      setProducts([...products, response.data]);
+      setNewProduct({ name: '', price: '', imageUrl: '' });
+      setShowForm(false);
+    } catch (error) {
+      console.error('Error adding product:', error);
+    }
+  };
+
+  const handleProductDelete = async (productId) => {
+    try {
+      await axios.delete(`http://localhost:8070/api/products/${productId}`);
+      setProducts(products.filter(product => product.id !== productId));
+    } catch (error) {
+      console.error('Error deleting product:', error);
+    }
+  };
+
+  const handleUpdateProduct = (productId) => {
+   // Buraya güncelleme işlemlerini ekleyin
+  };
+
   return (
-    <div className="home-container"> {/* Ana konteynırı sarmalayın */}
-      <header>
-        <h2>Home</h2>
-        <p>Welcome to our online store!</p>
+    <div className="home-container">
+      <header className="header-container">
+        <h2>Welcome to Homepage</h2>
+        <div>
+          <a href="#" onClick={() => setShowForm(true)}>Create Product</a>
+          <a href="#" onClick={handleLogout}>Logout</a>
+        </div>
       </header>
-      <section className="products-container"> {/* Ürünleri sarmalayan konteynır */}
+      <section className="products-container">
         <h3>Products</h3>
         <div className="product-list">
           {products.map(product => (
-            <div key={product.id} className="product-card"> {/* Her ürünü kart olarak düzenleyin */}
-              <img className="product-image" src={product.imageUrl} alt={product.name} /> {/* Ürün resmi */}
+            <div key={product.id} className="product-card">
+              <img className="product-image" src={product.imageUrl} alt={product.name} />
               <div className="product-info">
                 <strong>{product.name}</strong>
                 <span>${product.price}</span>
               </div>
               <button onClick={() => handleAddToCart(product.id)}>Add to Cart</button>
+              <button onClick={() => handleUpdateProduct(product.id)}>Update</button>
+              <button onClick={() => handleProductDelete(product.id)}>Delete</button>
             </div>  
           ))}
         </div>
+        {showForm && (
+          <div className="form-container">
+            <h3>Create New Product</h3>
+            <form onSubmit={handleProductSubmit}>
+              <input 
+                type="text" 
+                placeholder="Product Name" 
+                value={newProduct.name} 
+                onChange={(e) => setNewProduct(prevState => ({ ...prevState, name: e.target.value }))} 
+              />
+              <input 
+                type="text" 
+                placeholder="Price" 
+                value={newProduct.price} 
+                onChange={(e) => setNewProduct(prevState => ({ ...prevState, price: e.target.value }))} 
+              />
+              <input 
+                type="text" 
+                placeholder="Image URL" 
+                value={newProduct.imageUrl} 
+                onChange={(e) => setNewProduct(prevState => ({ ...prevState, imageUrl: e.target.value }))} 
+              />
+              <div className="button-group">
+                <button type="submit">Add Product</button>
+                <button onClick={() => setShowForm(false)}>Cancel</button>
+              </div>
+            </form>
+          </div>
+        )}
+      
       </section>
-      <footer> <button onClick={handleLogout}>Logout</button></footer>
+    
     </div>
   );
-}
+};
 
-export default Home;
+export default HomePage;
